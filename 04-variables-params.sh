61f268ec9e616ecb2709f3096aaab6bdcd43be72
kedarnath9988 <anubolukedarnath9988@gmail.com>
2024-10-02 21:16:06 +0530

    1

---
+++ 04-variables-params.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+Person1=$1
+Person2=$2
+
+
+echo " $Person1 : hi Suresh , How are you "
+echo " $Person2 : me good what about you $Person1 "
+echo " $Person1 : good , How is work going on"
+echo " $Person2 : am upgrading to Shell $Person1  " 
