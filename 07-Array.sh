ba8f36b12728e353f2205f00d71fbdfc170c817a
kedarnath9988 <anubolukedarnath9988@gmail.com>
2024-10-02 21:36:18 +0530

    1

--- 07-Array.sh
+++ 07-Array.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+#Array no commas ok 
+# with in the double codes 
+
 movies=("RRR" "RC" "NTR")
 
-echo "ist movie is ${movies[0]} "
+echo "1st movie is ${movies[0]} "
 echo "2st movie is ${movies[1]} "
 echo "all  movie are  ${movies[@]}"
