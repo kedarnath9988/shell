0b32471bd792e8ae1279d741296df234381f1ade
kedarnath9988 <anubolukedarnath9988@gmail.com>
2024-10-02 21:24:25 +0530

    1

--- 06-confidentials.sh
+++ 06-confidentials.sh
@@ -4,7 +4,7 @@ echo "please enter user-name"
 
 read USERNAME
 
-echo " please enter the password"
+echo "please enter the password"
 read  -s PASSWORD 
 
 echo " user is $USERNAME and password is $PASSWORD " 
