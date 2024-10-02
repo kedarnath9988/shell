192cbf4b08e6e31be05d336edd248a88520bca17
kedarnath9988 <anubolukedarnath9988@gmail.com>
2024-10-02 21:15:05 +0530

    1

---
+++ 03-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+Person1="Ramesh"
+Person2="Suresh"
+
+
+echo " $Person1 : hi Suresh , How are you "
+echo " $Person2 : me good what about you $Person1 "
+echo " $Person1 : good , How is work going on"
+echo " $Person2 : am upgrading to Shell $Person1  " 
