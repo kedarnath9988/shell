af6ea0a05f1925481ee27edde395977faf125a31
kedarnath9988 <anubolukedarnath9988@gmail.com>
2024-10-02 21:12:13 +0530

    1

---
+++ variable-02.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo " Ramesh : hi Suresh , How are you"
+echo " Suresh : me good what about you "
+echo " Ramesh : good , How is work going on"
+echo " Suresh : am upgrading to Shell " 
