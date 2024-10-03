76bf9156b627ec03a7f90f9a99ee0b8b64c06a75
kedarnath9988 <anubolukedarnath9988@gmail.com>
2024-10-03 22:29:22 +0530

    mysql.sh-file

--- mysql.sh
+++ mysql.sh
@@ -3,16 +3,41 @@
 USER=$( id -u )
 TIME_STAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE=$SCRIPT_NAME-$TIME_STAMP.log
-echo "current script name is :"$LOG_FILE
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log
 
+# enabling the colours 
+R="\e[31m"
+Y="\e[32m"
+G="\e[33m"
+N="\e[0m"
 
 if [ $USER -eq 0 ]
 then    
-    echo " you are the super user"
+    echo  -e "$G  you are the super user $N "
 else    
-    echo " you needed super user access to do this"
+    echo -e  "$R you needed super user access to do this $N"
     ecit 1 # manually exiting 
 fi 
 
+# redireccting by defautly only success command is redirecting to the log file by giving 1> 
+# redireccting  failure  command is redirecting to the log file by giving 2>
+# But we need to redirecting both the success and failure must provide &>> 
+
+
+validate()
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$G $2 done successfully ..$N"
+    else 
+        echo  -e "$R $2  failure $N "
+    fi 
+
+dnf install mysql-server -y &>> $LOG_FILE
+validate S? "installing mysql-server"
+
+systemctl enable mysql  &>> $LOG_FILE
+validate $? "enabling mysql"
+
+systemctl start mysql &>> $LOG_FILE
+validate S? "starting mysql"
 
