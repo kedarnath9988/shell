4056df499405bfd90dfebfa4594dca27f06d95f8
kedarnath9988 <anubolukedarnath9988@gmail.com>
2024-10-03 18:21:22 +0530

    2nd

--- 12-function.sh
+++ 12-function.sh
@@ -4,30 +4,27 @@ USER=$( id -u )
 TIME_STAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$( "echo $0 | cut -d "." -f1" )
 LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log 
+# enabling colours 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 validate(){
     if [ $1 -eq 0 ]
     then 
-        echo "instaling $2 Successfully "
+        echo -e  " $G instaling $2 Successfully $N  "
     else
-        echo " instaling $2 failed"
+        echo -e  " $R  instaling $2 failed $N "
         exit 1 
     fi 
 }
-validate(){
-    if [ $1 -eq 0 ]
-    then 
-        echo "instaling $2 Successfully "
-    else
-        echo " instaling $2 failed"
-        exit 1 
-    fi 
-}
+
 if [ $USER -eq 0 ]
 then
-    echo " you are super user"
+    echo -e  " $G  you are super user $N "
 else
-    echo " need super-user Access to do "
+    echo  -e " $R  need super-user Access to do $N  "
     exit 1 
 fi 
 
