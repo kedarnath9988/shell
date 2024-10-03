5cd70930e53fc9dc7a0c756891a746ec56c631e2
kedarnath9988 <anubolukedarnath9988@gmail.com>
2024-10-03 22:16:54 +0530

    mysql.sh-file

---
+++ mysql.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+USER=$( id -u )
+TIME_STAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+
+echo "current script name is :"$SCRIPT_NAME
+
+
+if [ $USER -eq 0 ]
+then    
+    echo " you are the super user"
+else    
+    echo " you needed super user access to do this"
+    ecit 1 # manually exiting 
+fi 
+
+
