d8f53ddecd9c05f003ccf82265bf62e0b6ebe95b
kedarnath9988 <anubolukedarnath9988@gmail.com>
2024-10-03 07:52:05 +0530

    2nd

--- 09-special_varuables.sh
+++ 09-special_varuables.sh
@@ -2,8 +2,14 @@
 
 echo "to print all  params "$@
 
+echo " count no of  params"$#
+
 echo "to print the shell script name ia "$0
 
-echo "to knoe the current user "$user
+echo "to knoe the current user "$USER
 
 echo "to konw the current working directory " $PWD 
+
+echo " to know the pid of the current shell script is "$$ 
+
+
