691de8bf5036b2352640f85e6396f7f42f02306d
kedarnath9988 <anubolukedarnath9988@gmail.com>
2024-10-03 18:23:19 +0530

    2nd

--- 12-function.sh
+++ 12-function.sh
@@ -2,7 +2,7 @@
 
 USER=$( id -u )
 TIME_STAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$( "echo $0 | cut -d "." -f1" )
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
 LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log 
 # enabling colours 
 R="\e[31m"
