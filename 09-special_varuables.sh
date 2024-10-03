e686565e45f49fd8b3f3d35921b62615f8a44f36
kedarnath9988 <anubolukedarnath9988@gmail.com>
2024-10-03 07:50:12 +0530

    2nd

--- 09-special_varuables.sh
+++ 09-special_varuables.sh
@@ -1,5 +1,9 @@
 #!/bin/bash 
 
-echo "total no of params "$@
+echo "to print all  params "$@
 
-echo "to print the shell script name ia $0"
+echo "to print the shell script name ia "$0
+
+echo "to knoe the current user "$user
+
+echo "to konw the current working directory " $PWD 
