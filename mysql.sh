cfbc17e73063b8ef670d402edbdb6b53d3747ad5
kedarnath9988 <anubolukedarnath9988@gmail.com>
2024-10-03 22:18:57 +0530

    mysql.sh-file

--- mysql.sh
+++ mysql.sh
@@ -4,7 +4,7 @@ USER=$( id -u )
 TIME_STAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE=$SCRIPT_NAME-$TIME_STAMP.log
-echo "current script name is :"$SCRIPT_NAME
+echo "current script name is :"$LOG_FILE
 
 
 if [ $USER -eq 0 ]
