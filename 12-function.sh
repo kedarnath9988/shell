3e939749b796a466070d6c8e59fd0cd842aa33d6
kedarnath9988 <anubolukedarnath9988@gmail.com>
2024-10-03 17:47:59 +0530

    2nd

--- 12-function.sh
+++ 12-function.sh
@@ -1,6 +1,41 @@
 #!/bin/bash
 
 USER=$( id -u )
-TIMESTAMP=$(date +%F-%H-%M-%S)
+TIME_STAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log 
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log 
+
+validate(){
+    if [ $1 -eq 0 ]
+    then 
+        echo "instaling $2 Successfully "
+    else
+        echo " instaling $2 failed"
+        exit 1 
+    fi 
+}
+validate(){
+    if [ $1 -eq 0 ]
+    then 
+        echo "instaling $2 Successfully "
+    else
+        echo " instaling $2 failed"
+        exit 1 
+    fi 
+}
+if [ $USER -eq 0 ]
+then
+    echo " you are super user"
+else
+    echo " need super-user Access to do "
+    exit 1 
+fi 
+
+dnf install git -y  
+validate $? "intalling git"  &>> $LOG_FILE
+
+dnf install nginx -y 
+validate $? "intalling nginx" &>> $LOG_FILE
+
+dnf install maven -y 
+validate $? "intalling maven" &>> $LOG_FILE
