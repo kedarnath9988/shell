d36456f683bd20f6e74b8f6e5c57c9480d62f855
kedarnath9988 <anubolukedarnath9988@gmail.com>
2024-10-03 11:01:06 +0530

    2nd

--- 12-function.sh
+++ 12-function.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 USER=$( id -u )
-TIMESTAMP=$( date +%F-%H-%M-%S )
-SCRIPT_NAME=(echo $0 | cut -d "." -f1)
+TIMESTAMP=$( $date +%F-%H-%M-%S )
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 
 echo "time "  $TIMESTAMP
-echo " current script nam eis " $SCRIPT_NAME
+echo " current script name is " $SCRIPT_NAME
