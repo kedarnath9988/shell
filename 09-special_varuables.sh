e5198d4cd91711767948e1a6d4ca21d8ee1c11ee
kedarnath9988 <anubolukedarnath9988@gmail.com>
2024-10-03 08:03:06 +0530

    2nd

--- 09-special_varuables.sh
+++ 09-special_varuables.sh
@@ -10,15 +10,19 @@ echo "to knoe the current user "$USER
 
 echo "to konw the current working directory " $PWD 
 
+echo " to know the home directory of the current user is " $HOME 
+
+echo "to know the hostname of the OS is " $HOST
+
 echo " to know the pid of the current shell script is "$$ 
 
 sleep 10 
 
-echo "to know the pid of the previous commad is " $? 
+echo "to know the exit-status  previous commad is " $? 
 
 touch hello.txt 
 
-echo "to know the pid of the previous commad is " $? 
+echo "to know the exit-status previous commad is " $? 
 
 
 
