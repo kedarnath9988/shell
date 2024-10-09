012d94e5e7d3c1767d4ca0d67d6027b5f6231328
kedarnath9988 <anubolukedarnath9988@gmail.com>
2024-10-09 09:53:53 +0530

    16 & 15 files

--- 15-current-Script.sh
+++ 15-current-Script.sh
@@ -3,9 +3,11 @@
 COURSE="DevOps for the current script"
 
 echo "before calling the other script "$COURSE
-echo "pid of  the current script"$$
+echo "pid of  the current script" $$
 
-./16-another-script.sh
+#./16-another-script.sh
+
+source ./16-another-script.sh
 
 echo "After calling the other script "$COURSE
 echo "pid of  the current script" $$
